fc684a35b3cb71498ef9d4e64ef97d1d54d12642
Nirvacsh <nir.vlad.zu@gmail.com>
2023-08-10 11:53:32 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { useState } from 'react'
+import data from './data.js'
+import Questions from './Questions.jsx'
 const App = () => {
-  return <h2>Accordion Starter</h2>;
-};
-export default App;
+  const [questions, setQuestions] = useState(data)
+  const [activeId, setActiveId] = useState(null)
+
+  const toggleQuestion = (id) => {
+    const newActiveId = activeId === id ? null : id
+    setActiveId(newActiveId)
+  }
+  return (
+    <main>
+      <Questions
+        questions={questions}
+        activeId={activeId}
+        toggleQuestion={toggleQuestion}
+      />
+    </main>
+  )
+}
+export default App
